41c436fbc9b34f58c313436f82224280d8f100ce
AngeloC89 <Angelociulla89@gmail.com>
2024-03-27 15:22:22 +0100

    added red class on h1

--- js/script.js
+++ js/script.js
@@ -4,13 +4,24 @@ const { createApp } = Vue
 
 //option object.
 createApp({
-  data() { 
+  data() {
     return {
-      message: '<h1>Hello Vue</h1>',     
+      message: '<h1>Hello Vue</h1>',
       image: '/img/1.jpg',
-      alt: 'Angela Carrol'
+      alt: 'Angela Carrol',
+      myclass: 'red',
+
+    }
+  },
+  methods: {
+    color() {
+      if (myclass === 'red') {
+        myclass = 'blue';
+
+      } else {
+        myclass = 'red';
+      }
     }
   }
-
 
 }).mount('#app')
